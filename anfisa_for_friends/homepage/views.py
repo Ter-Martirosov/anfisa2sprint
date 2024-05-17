bff7707eb0d71d17a5b804be4c4c0d7761fb6bd8
Ter-Martirosov <arkady.ter-martirosov@yandex.ru>
2024-05-17 18:56:46 +0300

    add interactions with database via views-functions

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,12 @@
+from django.db.models import Q
 from django.shortcuts import render
+from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values('id', 'title', 'description', 'price', 'category__is_published').filter(Q(is_published=True) & Q(is_on_main=True) & Q(category__is_published=True))[:3]
+    context = {
+        'ice_cream_list': ice_cream_list
+    }
+    return render(request, template, context)
