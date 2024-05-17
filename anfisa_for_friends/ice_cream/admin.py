3d22c831fa02d37e391444cdae751a4c118daffd
Ter-Martirosov <arkady.ter-martirosov@yandex.ru>
2024-05-17 03:17:35 +0300

    Create admin-zone

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Wrapper, Topping, IceCream
+
+admin.site.register(Wrapper)
+admin.site.register(Topping)
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+
+# Регистрируем кастомное представление админ-зоны
+admin.site.register(IceCream, IceCreamAdmin)
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
